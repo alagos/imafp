221587d0e61033035c122a87d294cb65873c524f
Alter Lagos <alter.strife@gmail.com>
2012-08-14 18:14:29 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imafp::Application.config.secret_token = '996fcfb2b0e45696b7de78d8d1baa2f07aa918a159f8db674ce5b44f597f7aa59537d3aec153bd3f5acaf466ee00d6204cdc7475d81c1931a6533b0a4ab292ed'
